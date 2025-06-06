c235317c044926371b4b3a9fb465a4edf9623b3c
松浦 <2210344@ecc.ac.jp>
2025-06-07 01:34:59 +0900

    first commit

---
+++ Neshinavi2/Neshinavi2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Neshinavi2
+//
+//  Created by 松浦壮吾 on 2025/06/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Neshinavi2/Neshinavi2/Neshinavi2App.swift
@@ -0,0 +1,17 @@
+//
+//  Neshinavi2App.swift
+//  Neshinavi2
+//
+//  Created by 松浦壮吾 on 2025/06/07.
+//
+
+import SwiftUI
+
+@main
+struct Neshinavi2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
