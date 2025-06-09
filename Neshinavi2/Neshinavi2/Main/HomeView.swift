a24e7005386f81977cd115a0ebf83e91c1f0d5ca
松浦 <2210344@ecc.ac.jp>
2025-06-09 22:23:06 +0900

    add: homeview追加

---
+++ Neshinavi2/Neshinavi2/Main/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Neshinavi2
+//
+//  Created by 松浦壮吾 on 2025/06/09.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
